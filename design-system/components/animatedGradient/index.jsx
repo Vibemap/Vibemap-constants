21bcaa3691535ecb7e3ba135cb0b98c849bcbf66
Steve Pepple <stephen.pepple@gmail.com>
2021-03-09 13:33:12 -0800

    Make react the same as vibemap.com

--- design-system/components/animatedGradient/index.jsx
+++ design-system/components/animatedGradient/index.jsx
@@ -4,11 +4,7 @@ import SVG from 'react-inlinesvg'
 import styles from './animatedGradient.scss'
 
 const AnimatedGradient = (props) => {
-    // Follows the specification for images returned from the Vibemap database.
-    useEffect(() => {
-        setCSS()
-    })
-
+        
     const setCSS = () => {
         let { 
             blur, 
@@ -17,24 +13,45 @@ const AnimatedGradient = (props) => {
             color3,
             color4,            
             waveLevel 
-        } = this.props
+        } = props
 
         console.log('setCSS: ', color1, color2, color3, color4)
         if (waveLevel === 'low') blur *= 2
-        
-        document.documentElement.style.setProperty('--color-1', color1)
-        document.documentElement.style.setProperty('--color-2', color2)
-        document.documentElement.style.setProperty('--color-3', color3)
-        document.documentElement.style.setProperty('--color-4', color4)
-        document.documentElement.style.setProperty('--blur', blur + 'px')
+
+        try {
+            document.documentElement.style.setProperty('--color-1', color1)
+            document.documentElement.style.setProperty('--color-2', color2)
+            document.documentElement.style.setProperty('--color-3', color3)
+            document.documentElement.style.setProperty('--color-4', color4)
+            document.documentElement.style.setProperty('--blur', blur + 'px')       
+        } catch (error) {
+            console.log('Problem setting document var: ', error)
+        }
     }
 
-    let { className, height, src, size, waveLevel, vibemap_images } = this.props
+
+    // Follows the specification for images returned from the Vibemap database.
+    // FIXME: Should be hook, but that's not working...
+    setCSS()
+
+    let { className, height, src, size, waveLevel, vibemap_images } = props
 
     let svg = '/svgs/' + waveLevel + '.svg'
 
     return (
-        <div>test</div>
+        <div 
+            className={"animatedGradient" + " " + className}
+            style={{height: height, width: '100%'}}>
+            <div className='noise'></div>
+            <div className='color'></div>            
+            <div className='blur'>
+                <div className={'wave ' + waveLevel}>
+                    <SVG 
+                        src={svg}
+                        height='auto'/>  
+                </div>
+            </div>
+        </div>
     )
 }
 
