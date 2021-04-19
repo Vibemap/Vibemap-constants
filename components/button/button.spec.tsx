b3002fd724109906f3a4f12611933d5c7be290d1
Steve Pepple <stephen.pepple@gmail.com>
2021-04-19 16:24:24 -0700

    Working bit harmony config and first component

---
+++ components/button/button.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BasicButton } from './button.composition';
+
+describe('button', () => {
+  it('should render with the correct text', () => {
+    const { getByText } = render(<BasicButton />);
+    const rendered = getByText('hello from Button');
+    expect(rendered).toBeTruthy();
+  });
+});
