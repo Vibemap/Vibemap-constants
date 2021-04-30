93e9a2497c16dc5e92be04fec49e6d3972ad38b4
Steve Pepple <stephen.pepple@gmail.com>
2021-04-29 17:30:02 -0700

    Add text component with type props

--- design-system/components/text/Text.stories.tsx
+++ design-system/components/text/Text.stories.tsx
@@ -2,20 +2,34 @@ import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
 
-import Text, { TextProps } from "./text";
+import Text, { Sizes, Weights, TextProps } from "./text";
 
 export default {
   title: "Components/Text",
-  component: t,
-  argTypes: {},
+  component: Text,
+  argTypes: {
+    size: {
+      control: {
+        type: 'radio',
+        options: Object.values(Sizes)
+      }
+    },
+    weight: {
+      control: {
+        type: 'radio',
+        options: Object.values(Weights)
+      }
+    }
+  },
 } as Meta;
 
 // Create a master template for mapping args to render the Text component
-const Template: Story<TextProps> = (args) => <Text {...args} />;
+const Template: Story<TextProps> = (args) => <Text {...args} >{args.text}</Text>;
 
 // Reuse that template for creating different stories
 export const Normal = Template.bind({});
-Normal.args = { label: "Primary 😃", size: "large" };
-
-export const Small = Template.bind({});
-Small.args = { ...Primary.args, primary: false, label: "Secondary 😇" };
+Normal.args = {
+  size: Sizes.large,
+  weight: Sizes.normal,
+  text: "Example"
+}
---
+++ design-system/components/text/text.tsx
@@ -0,0 +1,57 @@
+import React, { FunctionComponent } from 'react';
+
+import { Text, StyleSheet } from 'react-native';
+import styled from 'styled-components';
+
+import tokens from 'vibemap-constants/design-system/build/json/variables.json'
+
+// 'small' | 'normal' | 'large'
+export enum Sizes{
+  small = 'small',
+  normal = 'normal',
+  large = 'large'
+}
+
+export enum Weights{
+  light = 'light',
+  normal = 'normal',
+  medium = 'medium',
+  bold = 'bold'
+}
+
+export interface TextProps {
+  disabled?: boolean
+  size?: Sizes
+  weight?: Weights,
+  text: any
+}
+
+// TODO: Make this a util
+const getKeyValue = <T extends object, U extends keyof T>(obj: T) => (key: U) =>
+  obj[key];
+
+const TextElement:  React.FC<TextProps> = (props) => {
+  // De-structured, Default props
+  const {
+    children,
+    size = Sizes.normal,
+    weight = Weights.normal} = props
+
+  console.log('Text size ', size, getKeyValue(tokens.font.size)(size))
+
+  const styles = StyleSheet.create({
+    box: { padding: 10 },
+    text: {
+      fontWeight: getKeyValue(tokens.font.weight)(weight),
+      fontSize: getKeyValue(tokens.font.size)(size)
+    }
+  })
+
+  return (
+    <Text style={styles.text}>
+      {props.children}
+    </Text>
+  );
+}
+
+export default TextElement
